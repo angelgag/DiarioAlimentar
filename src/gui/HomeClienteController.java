1030479dc53ae7908c3527ba8e89409eecd9e88b
pedro <pmesquitabr@gmail.com>
2021-07-09 00:11:19 -0300

    home do usuário com transições

---
+++ src/gui/HomeClienteController.java
@@ -0,0 +1,56 @@
+package gui;
+
+import java.io.IOException;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.stage.Stage;
+
+public class HomeClienteController {
+    private Scene scene;
+    private Stage stage;
+	
+    @FXML
+    private Label lbHome;
+    
+	@FXML
+    private Button btnDiarioAlimentar;
+
+    @FXML
+    private Button btnPlanoAlimentar;
+
+    @FXML
+    void btnDiarioAlimentarClick(ActionEvent event) {
+    	try {
+        	Parent root = FXMLLoader.load(getClass()
+        			.getResource("DiarioAlimentar.fxml"));
+        	stage = (Stage)((Node)event.getSource()).getScene().getWindow();
+        	this.scene = new Scene(root);
+        	stage.setScene(scene);
+        	stage.show();
+    	} catch (IOException e) {
+    		e.printStackTrace();
+    	}
+    }
+
+    @FXML
+    void btnPlanoAlimentarClick(ActionEvent event) {
+    	try {
+        	Parent root = FXMLLoader.load(getClass()
+        			.getResource("PlanoAlimentar.fxml"));
+        	stage = (Stage)((Node)event.getSource()).getScene().getWindow();
+        	this.scene = new Scene(root);
+        	stage.setScene(scene);
+        	stage.show();
+    	} catch (IOException e) {
+    		e.printStackTrace();
+    	}
+    }
+
+}
--- src/gui/LoginController.java
+++ src/gui/LoginController.java
@@ -1,8 +1,5 @@
 package gui;
 import java.io.IOException;
-import java.time.LocalDate;
-import java.util.List;
-
 import beans.Cliente;
 import beans.Nutricionista;
 import javafx.event.ActionEvent;
@@ -41,10 +38,6 @@ public class LoginController {
     @FXML
     private Button btnCadastro;
     
-    //private int autenticacao;
-    private List<Cliente> clientes = RepositorioCliente.getInstance().recover();
-    private List<Nutricionista> nutricionistas = RepositorioNutricionista.getInstance().recover();
-    
     @FXML
     void cadastro(ActionEvent event) {
 
@@ -78,12 +71,7 @@ public class LoginController {
     			String senhaR = n.getSenha();
     			String emailI = txtEmail.getText();
     			String senhaI = txtSenha.getText();
-    			System.out.println(emailR.length());
-    			System.out.println(emailI.length());
-    			System.out.println(senhaR.length());
-    			System.out.println(senhaI.length());
     			if (emailR.equals(emailI) && senhaR.equals(senhaI)) {
-    				System.out.println("erro ao tentar logar");
     	    		verificar = true;
     	        	
     	    		try {
@@ -98,34 +86,37 @@ public class LoginController {
     	        	}
     	    		break;
     	    	} 
-    	    	}
+    	    }
+    		for (Cliente c : RepositorioCliente.getInstance().recover()) {
+    			String emailR = c.getEmail();
+    			String senhaR = c.getSenha();
+    			String emailI = txtEmail.getText();
+    			String senhaI = txtSenha.getText();
+    			if (emailR.equals(emailI) && senhaR.equals(senhaI)) {
+    	    		verificar = true;
+    	        	
+    	    		try {
+    	            	BorderPane homeClientePane = FXMLLoader.load(getClass()
+    	            			.getResource("HomeCliente.fxml"));
+    	            	stage = (Stage)((Node)event.getSource()).getScene().getWindow();
+    	            	this.scene = new Scene(homeClientePane);
+    	            	stage.setScene(scene);
+    	            	stage.show();
+    	        	} catch (IOException e) {
+    	        		e.printStackTrace();
+    	        	}
+    	    		break;
+    	    	} 
+    	    }
     	}
     	if (verificar == false) {
         	Alert alert = new Alert(AlertType.CONFIRMATION);
         	alert.setTitle("Erro ao tentar logar");
         	alert.setContentText("Email e senha incorretos, digite novamente");
     	} else {
-    		System.out.println("erro ao tentar logaaaaar");
+    		System.out.println("login efetuado com sucesso");
     	}
-    	/*
-    	for (Cliente c : this.clientes) {
-    	if (c.getEmail() == txtEmail.getText() && c.getSenha() == txtSenha.getText()) {
-    		
-    		verificar = true;
-        	
-    		try {
-            	BorderPane diarioAlimentarPane = FXMLLoader.load(getClass()
-            			.getResource("DiarioAlimentar.fxml"));
-            	stage = (Stage)((Node)event.getSource()).getScene().getWindow();
-            	this.scene = new Scene(diarioAlimentarPane);
-            	stage.setScene(scene);
-            	stage.show();
-        	} catch (IOException e) {
-        		e.printStackTrace();
-        	}
-    		break;
-    	} 
-    	}*/
+
     }
     
 }
